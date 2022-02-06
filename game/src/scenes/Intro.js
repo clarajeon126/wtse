7daa0459589d0d2d2c09c8030e99550474ebd514
helliechen <helliechen2@gmail.com>
2022-02-06 18:03:04 -0500

    next buttonchanging

--- game/src/scenes/Intro.js
+++ game/src/scenes/Intro.js
@@ -1,9 +1,10 @@
 import Phaser from "phaser";
 import baseboard from '../assets/passets/baseboard.png';
 import nextbutton from "../assets/passets/nextbutton.png";
-import fontpng from "../assets/font.png";
-import fontxml from "../assets/font.xml";
-
+import fontpng from "../assets/Unnamed.png";
+import fontxml from "../assets/Unnamed.xml";
+// import fontpng from "../assets/minecraftia.png";
+// import fontxml from "../assets/minecraftia.xml";
 export default class Intro extends Phaser.Scene
 {
     
@@ -18,11 +19,6 @@ export default class Intro extends Phaser.Scene
     {
         this.load.image('nextbutton', nextbutton);
         this.load.image('baseboard', baseboard);
-        // this.load.bitmapFont(
-        //     'font',
-        //     fontpng,
-        //     fontxml
-        //   );
         this.load.bitmapFont(
                 'font',
                 fontpng,
@@ -33,23 +29,33 @@ export default class Intro extends Phaser.Scene
 
     create ()
     {
-        this.add.image(100, 54, 'baseboard');
-        this.add.image(130,80, 'nextbutton');
+        let text = ["welcome to where the sidewalk ends",
+        "in this game, you'll be controlling a lanky biped who's on a mission!",
+        "what this mission is you ask?",
+        "of course to tell your mom [SOMETHING]",
+        "press the left and right keys to time your steps",
+        "the better timed your steps the faster you can make it home!",
+        "however, as you walk along the sidewalk, beware of cracks!",
+        "make sure to jump over them with the space bar",
+        "...who knows what'll happen otherwise"];
 
-        const text = ["welcome to where the sidewalk ends",
-            "in this game, you'll be controlling a lanky biped who's on a mission!",
-            "what this mission is you ask?",
-            "of course to tell your mom [SOMETHING]",
-            "press the left and right keys to time your steps",
-            "the better timed your steps the faster you can make it home!",
-            "however, as you walk along the sidewalk, beware of cracks!",
-            "make sure to jump over them with the space bar",
-            "...who knows what'll happen otherwise"];
-        var infotext = this.add.text(20,10, text[1]);
-        // infotext.setScale(.01)
-        // infotext.setResolution(90);
-        // this.add.bitmapText(200,100, 'font', text[1])
-        //this.add.bitmapText(20, 10, 'font',text[1]);
+        var i = 0;
+
+        this.add.image(100, 54, 'baseboard');
+        const nextButton = this.add.image(130,80, 'nextbutton');
+        nextButton.setInteractive();
+
+        // this.infotext = this.add.text(60,30,text[i]);
+        // this.infotext.setScale('.5');
+
+        nextButton.on('pointerdown', () =>{
+            this.infotext.setText(text[++i]); 
+            console.log(i);});
+
+
+        this.texting = this.add.bitmapText(60,30, 'font', text[1]);
+        this.texting.setScale('.05');
+
         console.log('cool');
         
     }
