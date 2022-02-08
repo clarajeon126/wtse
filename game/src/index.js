1851a4c1eea1b7212a1888145092c2fdec009be7
helliechen <helliechen2@gmail.com>
2022-02-07 23:37:21 -0500

    stupid annoying buttons

--- game/src/index.js
+++ game/src/index.js
@@ -16,7 +16,7 @@ const C = {
             debug: true
         }
     },
-    scene: [Parallax, Text]
+    scene: [Intro,Parallax, Text]
 };
 
 // var C = {
--- game/src/scenes/Intro.js
+++ game/src/scenes/Intro.js
@@ -1,38 +1,130 @@
 import Phaser from "phaser";
+import fontpng from "../assets/minecraftia.png";
+import fontxml from "../assets/minecraftia.xml";
 import baseboard from '../assets/passets/baseboard.png';
+import intro from "../assets/passets/intro.png";
 import nextbutton from "../assets/passets/nextbutton.png";
-import fontpng from "../assets/pixel.png";
-import fontxml from "../assets/pixel.xml";
-// import fontpng from "../assets/minecraftia.png";
-// import fontxml from "../assets/minecraftia.xml";
-export default class Intro extends Phaser.Scene
-{
-    
-    constructor(){
+import homebutton from "../assets/passets/home.png";
+import resumebutton from "../assets/passets/resumebutton.png";
+import pausebutton from "../assets/passets/pausebutton.png";
+import restartbutton from "../assets/passets/restart.png";
+import playbutton from "../assets/passets/playbutton.png";
+
+
+export default class Intro extends Phaser.Scene {
+
+    constructor() {
         super({
             key: "Intro"
         })
     }
 
-    
-    preload ()
-    {
-        this.load.image('nextbutton', nextbutton);
+
+    preload() {
         this.load.image('baseboard', baseboard);
+        this.load.spritesheet('nextbutton', nextbutton, { frameWidth: 22, frameHeight: 12 });
         this.load.bitmapFont(
-                'font',
-                fontpng,
-                fontxml
-              );
+            'font',
+            fontpng,
+            fontxml
+        );
+
     }
 
 
-    create ()
-    {
+    create() {
+        let i =0;
+        var clicking = true;
 
-        // this.game.scale.resize(1920,1080)
+        this.baseboard = this.add.image(96, 54, 'baseboard')
+        this.nextbutton = this.physics.add.staticSprite(120, 80, 'nextbutton').setInteractive();
         
-        this.scene.launch('Text').launch('Parallax').remove('Intro')
+
+        var text = ["welcome to where the sidewalk ends",
+        "in this game, you'll be controlling a lanky biped who's on a mission!",
+        "what this mission is you ask?",
+        "of course to tell your mom [SOMETHING]",
+        "press the left and right keys to time your steps",
+        "the better timed your steps the faster you can make it home!",
+        "however, as you walk along the sidewalk, beware of cracks!",
+        "make sure to jump over them with the space bar",
+        "...who knows what'll happen otherwise"];
+
+        this.infotext = this.add.text(980,540,text[i]);
+
+        this.anims.create({
+            key: 'hover',
+            frames: this.anims.generateFrameNumbers('nextbutton', { frames: [1] }),
+            frameRate: 6,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'out',
+            frames: this.anims.generateFrameNumbers('nextbutton', { frames: [0] }),
+            frameRate: 6,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'click',
+            frames: this.anims.generateFrameNumbers('nextbutton', { frames: [2] }),
+            frameRate: 6,
+            repeat: 0
+        });
+
+        //when hover 
+        this.nextbutton.on('pointerover', function () {
+            console.log('pls work');
+            this.anims.play('hover', true)
+        });
+
+        //when not hovering
+        this.nextbutton.on('pointerout', function () {
+            this.anims.play('out', true)
+        });
+
+        //when clicking
+        this.nextbutton.on('pointerdown', ()=> 
+            
+         //this.anims.play('click', true),
+            this.updateText(++i),
+            this.changeFrame(true),
+
+        );
+
+
+
+
+        //scales every game object this is actually so big brain im godly 😎
+        this.group = this.add.group(this.children.list)
+        this.group.children.iterate((child, index) => {
+
+            //scale everything except for graphics
+            if (index != 0) {
+                console.log(child.type)
+
+                //cause if u scale text it looks mega huge
+                if (child.type != "Text") {
+                    child.setScale(10, 10)
+                    child.setX(child.x * 10)
+                    child.setY(child.y * 10)
+                }
+                else {
+
+                    //texts rnt centered in the middle but the "center" is top left so change that to middle
+                    child.setOrigin(0.5, 0.5)
+                }
+
+            }
+            //updates the physics body for all objects w physics so that the physics bodys rnt mini sized yk
+            if (child.body) {
+                child.body.updateFromGameObject()
+            }
+        });
+
+        
+        // this.game.scale.resize(1920,1080)
 
         // this.add.image(960, 520, 'baseboard').setScale(10)
         // this.add.image(1000,700, 'nextbutton').setScale(10)
@@ -52,11 +144,27 @@ export default class Intro extends Phaser.Scene
         // // this.add.bitmapText(200,100, 'font', text[1])
         // // this.add.bitmapText(20, 10, 'font',text[1]).setScale(.1)
         // console.log('cool');
-        
+
         // this.cursors = this.input.keyboard.createCursorKeys();
     }
-
-    update() {
+    changeFrame(){
+        //this.nextbutton.setFrame('hover',1);
+        //this.anims.play('click', true);
+        console.log('heya');
 
     }
+    updateText(i){
+        var text = ["welcome to where the sidewalk ends",
+        "in this game, you'll be controlling a lanky biped who's on a mission!",
+        "what this mission is you ask?",
+        "of course to tell your mom [SOMETHING]",
+        "press the left and right keys to time your steps",
+        "the better timed your steps the faster you can make it home!",
+        "however, as you walk along the sidewalk, beware of cracks!",
+        "make sure to jump over them with the space bar",
+        "...who knows what'll happen otherwise"];
+        this.infotext.setText(text[i]);
+    }
+    update() {
+        }
 }
